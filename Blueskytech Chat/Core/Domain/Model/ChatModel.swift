76ddd7eefbb3b38a3653db4acf8c73a5c23ddfe4
fardan <fardan.wardhana@gmail.com>
2025-01-25 11:43:32 +0700

    Adding Model
    Create and Adding file ChatModel

---
+++ Blueskytech Chat/Core/Domain/Model/ChatModel.swift
@@ -0,0 +1,14 @@
+//
+//  ChatModel.swift
+//  Blueskytech Chat
+//
+//  Created by Muhamad Fardan Wardhana on 25/01/25.
+//
+
+import Foundation
+
+struct GameModel: Equatable, Identifiable {
+    var id: String
+    var user: String
+    var message: String
+}
