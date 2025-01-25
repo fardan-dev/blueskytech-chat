d76d5dede90ca634b7135fdf1483178c5ffc7436
fardan <fardan.wardhana@gmail.com>
2025-01-25 08:40:28 +0700

    init project

---
+++ Blueskytech Chat/Blueskytech_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blueskytech_ChatApp.swift
+//  Blueskytech Chat
+//
+//  Created by Muhamad Fardan Wardhana on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Blueskytech_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
